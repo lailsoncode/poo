d091386d21109a2aacdd22a545cad23df7258e79
Lailson Herondino <157296747+lailsoncode@users.noreply.github.com>
2025-03-25 19:37:41 +0000

    Aula 1/2

---
+++ Aula02/aula2.py
@@ -0,0 +1,11 @@
+class Garrafa:
+    def __init__(self,value1):
+        self.modelo = value1
+        self.cor = None
+        self.volume = None
+        self.estampa = None
+        self.preco = None
+
+garrafa1 = Garrafa("Atack On Titan")
+
+print (garrafa1.modelo)
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # poo
 Programação Orientada a Objetos
+
+Produto
+Garrafa
+
+cor
+tamanho
+preço
+estampa
+código
